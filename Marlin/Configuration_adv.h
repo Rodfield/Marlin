4b5a191ec1ad7f517f9096471b5429ff8b2953f8
Rodfield <70966736+Rodfield@users.noreply.github.com>
2020-12-04 06:22:03 +0000

    Update Configuration_adv.h
    Change TMC settings taken from printer.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2229,7 +2229,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       580        //jp (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       560        //jp 580 (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
@@ -2245,7 +2245,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       580 //jp
+    #define Y_CURRENT       560 //jp 580
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2261,7 +2261,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       380 //jp
+    #define Z_CURRENT       580 //jp 380
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2269,7 +2269,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      380 //jp
+    #define Z2_CURRENT      580 //jp 380
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    16
     #define Z2_RSENSE         0.11
@@ -2515,7 +2515,7 @@
     // TMC2209: 0...255. TMC2130: -64...63
     #define X_STALL_SENSITIVITY  179  //jp 180
     //#define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY  //jp
-    #define Y_STALL_SENSITIVITY  148  //jp
+    #define Y_STALL_SENSITIVITY  169  //jp 148
     //#define Y2_STALL_SENSITIVITY Y_STALL_SENSITIVITY  //jp
     //#define Z_STALL_SENSITIVITY  8
     //#define Z2_STALL_SENSITIVITY Z_STALL_SENSITIVITY
